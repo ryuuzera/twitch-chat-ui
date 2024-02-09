25d4e69271bcdbf8a8fe4663749c4dbbe4006e81
ryuuzera <faag_@live.com>
2024-02-09 20:24:56 -0300

    fix issues

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -20,12 +20,15 @@ const Chat = ({ channel }: any) => {
 
     const handleMessage = (channel: any, userstate: { [x: string]: any }, message: any, self: any) => {
       if (self) return;
-
+      console.log(userstate);
+      console.log(userstate.badges);
       let modifiedMessage = message;
-
+      modifiedMessage = modifiedMessage.replace(
+        /\b(https?:\/\/\S+|www\.\S+)\b/g,
+        '<a class="text-blue-500 underline" href="$1" target="_blank">$1</a>'
+      );
       if (userstate.emotes) {
         const emoteIds = Object.keys(userstate.emotes);
-
         emoteIds.forEach((emoteId) => {
           const emoteInstances = userstate.emotes[emoteId];
 
@@ -33,44 +36,50 @@ const Chat = ({ channel }: any) => {
             const [start, end] = emoteInstance.split('-');
             const emoteCode = message.substring(parseInt(start, 10), parseInt(end, 10) + 1);
             const emoteUrl = `https://static-cdn.jtvnw.net/emoticons/v2/${emoteId}/default/dark/1.0`;
-            modifiedMessage = `<span style="display: flex; flex-direction:row; gap: 5px;">${modifiedMessage.replace(
+            modifiedMessage = `<div class="flex flex-row max-h-[24px] gap-1">${modifiedMessage.replace(
               emoteCode,
               `<img src='${emoteUrl}' /> `
-            )}`;
+            )}</ div>`;
           });
         });
       }
       modifiedMessage = modifiedMessage.replace(
         /(@\w+)/g,
-        '<span style="background-color: grey;" class="p-1 rounded-lg">$1</span>'
+        '<p class="inline-block px-1 rounded-lg bg-gray-400 text-center">$1</p>'
       );
-      modifiedMessage = modifiedMessage.replace(
-        /\b(https?:\/\/\S+|www\.\S+)\b/g,
-        '<a class="text-blue-500 underline" href="$1" target="_blank">$1</a>'
-      );
-      console.log(modifiedMessage);
       setMessages((prevMessages) => [
         ...prevMessages,
         () => (
-          <div key={Date.now()} style={{ display: 'flex', flexDirection: 'row', gap: '5px' }}>
-            <span key={Date.now() + 1} style={{ color: `${userstate['color'] ?? '#00FF7F'}` }}>
-              {userstate['display-name']}:
-            </span>{' '}
-            <span key={Date.now() + 2} dangerouslySetInnerHTML={{ __html: modifiedMessage }} />
-          </div>
+          <>
+            <span className='flex items-center '>
+              {userstate.badges?.moderator && (
+                <img
+                  style={{ height: 18, width: 18 }}
+                  src='https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/1'
+                />
+              )}
+              {userstate.badges?.partner && (
+                <img
+                  style={{ height: 18, width: 18 }}
+                  src='https://static-cdn.jtvnw.net/badges/v1/d12a2e27-16f6-41d0-ab77-b780518f00a3/1'
+                />
+              )}
+              <p key={Date.now() + 1} style={{ color: `${userstate['color'] ?? '#00FF7F'}` }}>
+                {userstate['display-name']}:
+              </p>
+            </span>
+            <span className='inline-block' dangerouslySetInnerHTML={{ __html: modifiedMessage }}></span>
+          </>
         ),
       ]);
     };
 
-    // Adiciona o ouvinte de mensagem ao cliente
     client.on('message', handleMessage);
 
-    // Conecta ao chat da Twitch
     client.connect().then(() => {
-      console.log('Conectado ao chat da Twitch');
+      console.log(`Conectado ao chat do(a) ${channel}`);
     });
 
-    // Limpa a conexão quando o componente é desmontado
     return () => {
       client.removeListener('message', handleMessage);
       client.disconnect();
@@ -78,12 +87,19 @@ const Chat = ({ channel }: any) => {
   }, []);
 
   return (
-    <div>
-      <h1 className='text-red-500'>Chat da Twitch</h1>
-      <div className='space-y-2'>
-        {messages.map((Message, index) => (
-          <Message />
-        ))}
+    <div className='min-h-screen'>
+      <div className='flex flex-col items-center justify-start h-full w-full'>
+        <div className='max-h-lg max-w-lg border-solid border-red-300'>
+          <div className='py-2 flex flex-row items-center justify-center'>
+            <h1 className='text-red-500 m-2'>Chat da Twitch</h1>
+          </div>
+
+          <div className='flex flex-col gap-2 '>
+            {messages.map((Message) => (
+              <Message />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
